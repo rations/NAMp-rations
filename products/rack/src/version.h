e229ead3b9b40a1d85636447a92e689bd8d7682c
rations <ehqcar@proton.me>
2026-09-21 15:15:54 +1000

    version 0.5.0

--- products/rack/src/version.h
+++ products/rack/src/version.h
@@ -10,8 +10,8 @@
 // module said 0.2.0 while every class still answered 0.1.0.1.
 #define MAJOR_VERSION_STR "0"
 #define MAJOR_VERSION_INT 0
-#define SUB_VERSION_STR "4"
-#define SUB_VERSION_INT 4
+#define SUB_VERSION_STR "5"
+#define SUB_VERSION_INT 5
 #define RELEASE_NUMBER_STR "0"
 #define RELEASE_NUMBER_INT 0
 #define BUILD_NUMBER_STR "1"
